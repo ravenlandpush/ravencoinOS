84f7c261c2e98c34a4d77011e5153eb519d0b955
push <47545876+ravenlandpush@users.noreply.github.com>
2020-01-11 21:17:25 +0000

    Update patch-ravencoinOS-nov-2019-rpi4.sh

--- patch-ravencoinOS-nov-2019-rpi4.sh
+++ patch-ravencoinOS-nov-2019-rpi4.sh
@@ -18,7 +18,7 @@ sudo systemctl stop ravend
 
 echo "Please wait patiently.... do not interupt me";
 echo "Don't worry your wallet wont be overwritten...";
-sleep 20;
+sleep 5;
 
 echo "Removing old installations..."
 rm -rf ~pi/raven-3.2.0-qt 
@@ -31,32 +31,31 @@ tar zxvf restricted-assets-patch-rpi4-raven3.3.0-qt-latest.tar.gz
 
 
 echo "deleting old binaries..."
-sudo su -
-rm /bin/ravend;rm /bin/raven-qt;rm /bin/raven-cli
+sudo rm /bin/ravend;rm /bin/raven-qt;rm /bin/raven-cli
 
 echo "replacing new binaries..."
-cp -rp ~pi/raven-3.3.0-qt/bin/* /bin;
+sudo cp -rp ~pi/raven-3.3.0-qt/bin/* /bin;
 
 echo "updating systemctl unit files..."; sleep 2;
 sudo sed -i "s,raven-3.2.0,raven-3.3.0,g" /etc/systemd/system/multi-user.target.wants/ravend.service
 sudo sed -i "s,raven-3.2.0,raven-3.3.0,g" /lib/systemd/system/ravend.service
 
 echo "refreshing systemctl unit files"; sleep 2;
-systemctl daemon-reload;
+sudo systemctl daemon-reload;
 
 
-sed -i 's/raven-3.2.0/raven-3.3.0/g' /home/pi/launchwalletnow.sh
+sudo sed -i 's/raven-3.2.0/raven-3.3.0/g' /home/pi/launchwalletnow.sh
 
 
-chown -R pi:pi /home/pi;
-mv /home/pi/raven-3.3.0-qt/bin/raven-qt /home/pi/raven-3.3.0-qt/
+sudo chown -R pi:pi /home/pi;
+sudo mv /home/pi/raven-3.3.0-qt/bin/raven-qt /home/pi/raven-3.3.0-qt/
 
 echo "add correct permissions and put raven-qt in correct path..."
-chmod +x /home/pi/raven-3.3.0-qt/*
-chmod +x /home/pi/raven-3.3.0-qt/bin/*
-chmod +x /bin/ravend; chmod +x /bin/raven-cli; chmod +x /bin/raven-qt;
+sudo chmod +x /home/pi/raven-3.3.0-qt/*
+sudo chmod +x /home/pi/raven-3.3.0-qt/bin/*
+sudo chmod +x /bin/ravend; chmod +x /bin/raven-cli; chmod +x /bin/raven-qt;
 
 
 echo "Attempting to restart ravend and ipfs services..."
-systemctl start ravend;
-systemctl start ipfs;
+sudo systemctl start ravend;
+sudo systemctl start ipfs;
